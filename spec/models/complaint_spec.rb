209e1fcb8c2df5bb3252a29de47e08e8ad22fab3
Jorge Felipe <jfss1@ifal.edu.br>
2021-05-16 18:13:33 -0300

    make it better

--- spec/models/complaint_spec.rb
+++ spec/models/complaint_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe Complaint, type: :model do
   it { is_expected.to belong_to(:user) }
 
   it { is_expected.to validate_presence_of :description }
-  it { is_expected.to validate_presence_of :user_id }
 
   it { is_expected.to respond_to(:description) }
   it { is_expected.to respond_to(:lat) }
